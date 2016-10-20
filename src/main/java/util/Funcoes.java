1d7beeef28b6c463fb67048f373f9286ee5c5663
pmm-ti <pmm-ti@pmm-ti-DEV>
2016-10-20 13:34:54 -0300

    Início do projeto

--- src/main/java/util/Funcoes.java
+++ src/main/java/util/Funcoes.java
@@ -13,12 +13,11 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 public class Funcoes {
     
-    
     public void Mensagem(String wmensa){
         
         FacesContext context = FacesContext.getCurrentInstance();
         
-     //   context.addMessage(null, new FacesMessage("Successful",  "Your message: " + wmensa) );
+        context.addMessage(null, new FacesMessage("Successful",  "Your message: " + wmensa) );
         context.addMessage(null, new FacesMessage(wmensa, ""));
     }        
         
