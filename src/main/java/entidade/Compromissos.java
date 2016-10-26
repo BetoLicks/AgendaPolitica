36bfa6e930a0daa66f9d3f1b8bd864eebb304e12
pmm-dev <pmm-dev@pmmdev>
2016-10-26 10:55:07 -0300

    Compromissos

---
+++ src/main/java/entidade/Compromissos.java
@@ -0,0 +1,38 @@
+package entidade;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.*;
+
+/**
+ *
+ * @author pmm-dev
+ */
+
+@Entity
+@Table(name="tab_compromissos")
+public class Compromissos implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    @Temporal(TemporalType.DATE)
+    private Calendar dtentrada;
+    
+    @Temporal(TemporalType.DATE)
+    private Calendar dtcompromisso;
+    
+    @Temporal(TemporalType.TIME)
+    private Calendar hrcompromisso;
+    
+    private String endereco;
+    private String descricao;
+    private Boolean realizado;
+    
+    
+    
+    
+    
+    
+}
